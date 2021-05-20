0a40eab8e601c3f3a276810e66578412f01c80a8
MadScientistA <anmolzagrawal@gmail.com>
2021-05-20 19:15:43 +0200

    Don't override device fingerprint

--- revengeos_taimen.mk
+++ revengeos_taimen.mk
@@ -17,11 +17,11 @@ PRODUCT_MANUFACTURER := Google
 PRODUCT_RELEASE_NAME := taimen
 
 # Override Fingerprint for Safetynet passing
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=taimen \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
+#PRODUCT_BUILD_PROP_OVERRIDES += \
+#    PRODUCT_NAME=taimen \
+#    PRIVATE_BUILD_DESC="redfin-user 11 RQ2A.210505.003 7255357 release-keys"
 
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
+#BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
 
 # RevengeOS maintainer
 REVENGEOS_BUILDTYPE := UNOFFICIAL
